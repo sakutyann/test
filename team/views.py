b225ea01bf93ed1fd665b549e0204f8388a2355a
teamF <tky2302075@stu.o-hara.ac.jp>
2024-11-01 12:33:34 +0900

    test

--- team/views.py
+++ team/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def helloworldfunction(request):
-          return render(request, 'index.html')
+          return render(request, 'loginandnewgest.html')
