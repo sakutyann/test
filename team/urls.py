aa0503f4974bdd5ac462f281ff3a51b6664b13d3
teamF <tky2302075@stu.o-hara.ac.jp>
2024-11-11 15:00:11 +0900

    yamada

--- team/urls.py
+++ team/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [path('login/', views.loginworldfunction),
                path('main/', views.mainworldfunction),
                path('account/', views.accountworldfunction),
