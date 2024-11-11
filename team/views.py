d415187f515c0c2389ed5a0a3864b6cb7a1dd1b6
teamF <tky2302075@stu.o-hara.ac.jp>
2024-11-11 09:30:17 +0900

    yamada

--- team/views.py
+++ team/views.py
@@ -50,3 +50,29 @@ def questokformworldfunction(request):
 def questerformworldfunction(request):
           return render(request, 'quester.html')
 
+def newaccountformworldfunction(request):
+          return render(request, 'newaccount.html')
+
+def questnowformworldfunction(request):
+          return render(request, 'questnow.html')
+
+def questnotformworldfunction(request):
+          return render(request, 'questnot.html')
+
+def questdoformworldfunction(request):
+          return render(request, 'questdo.html')
+
+def questgoformworldfunction(request):
+          return render(request, 'questgo.html')
+
+def questyesformworldfunction(request):
+          return render(request, 'questyes.html')
+
+def questoutformworldfunction(request):
+          return render(request, 'questout.html')
+
+
+def questfinformworldfunction(request):
+          return render(request, 'questfin.html')
+
+
