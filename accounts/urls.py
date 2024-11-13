cb3bf7e8df23f89de88bab2d64e729a5b33fd5fe
project <tky2302058@stu.o-hara.ac.jp>
2024-11-13 09:25:18 +0900

    accounts3s

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # viewsモジュールをインポート
-# from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 # from django.contrib.auth. import views as auth_views
 from . import views
 
@@ -16,17 +16,31 @@ urlpatterns = [
     # viewsモジュールのSignupViewsをインスタンス化する
     path('signup/', views.SignUpView.as_view(), name='signup'),
     
-    # サインアップ完了ページのビューの呼び出し
-    # http(s)://＜ホスト名＞/signup_succes｝へのアクセスに対して、
-    # viewsモジュールのSignupViewsをインスタンス化する
-    path('signup_success/', views.SignUpSuccessView.as_view(), name='signup_success'),
+    # # サインアップ完了ページのビューの呼び出し
+    # # http(s)://＜ホスト名＞/signup_succes｝へのアクセスに対して、
+    # # viewsモジュールのSignupViewsをインスタンス化する
+    # path('signup_success/', views.SignUpSuccessView.as_view(), name='signup_success'),
     
     
     # ログインのURL設定
     path('signin/', views.SignInView.as_view(), name='signin'),
+
+    
+    # main画面へ
+    path('main/', views.main_view, name="main"),
+    
+    # ログアウト確認
+    path('logout_check/', views.logout_view, name='logout_check'),
+    # ログアウト完了
+    path('logout_ok/', views.logoutok_view, name='logout_ok'),
+    
+    # アカウント情報閲覧
+    path('account_info/', views.AccountInfoView.as_view(), name='account_info'),
+
+
+
     
     # test
     path('test/', views.test, name="test"),
-    
 
 ]
